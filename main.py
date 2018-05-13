40783797bd883144c47cde74fd775def247b95c7
Jesyfox <jesyfoxbs@gmail.com>
2018-05-13 14:23:09 +0300

    bugfix with connections

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ pygame.init()
 def game_update():
     for Area_Row in Player_Area:
         for Area_block in Area_Row: (Area_block.render(),Area_block.refresh())
-    #Connect_ships()
 
 def Connect_ships():
-    Row = list(range(0,10))
-    Col = list(range(0,10))
+    Ten = list(range(0,10))
 
-    for j in Col:
-        for i in Row:
-        	#if left and right are neighbours - connect it
+    for j in Ten:
+        for i in Ten:
+            #if left and right are neighbours - connect it
             if Player_Area[j][i].health_status and Player_Area[j-1][i].health_status:
                 Player_Area[j][i].ship.connect_Left = True
                 Player_Area[j-1][i].ship.connect_Right = True
@@ -23,15 +21,19 @@ def Connect_ships():
             if Player_Area[j][i].health_status and Player_Area[j][i-1].health_status:
                 Player_Area[j][i].ship.connect_Up = True
                 Player_Area[j][i-1].ship.connect_Down = True
-
-    for loc in Row:
             try:
-                Player_Area[0][loc].ship.connect_Left = False
-                Player_Area[-1][loc].ship.connect_Right = False
-                Player_Area[loc][0].ship.connect_Up = False
-                Player_Area[loc][-1].ship.connect_Down = False
+                Player_Area[0][i].ship.connect_Left = False
+                Player_Area[-1][i].ship.connect_Right = False
             except AttributeError:
                 continue
+            try:
+                Player_Area[i][0].ship.connect_Up = False
+            except AttributeError:
+                pass
+            try:
+                Player_Area[i][-1].ship.connect_Down = False
+            except AttributeError:
+                pass
 
 def placement():
     '''func that count ships and sections '''
@@ -50,6 +52,8 @@ pygame.display.set_caption("Warship battle!")
 Player_Area = Objects.draw_area(25, 25, 23, Objects.Location, screen) #Main player home grid
 
 Objects.Battle_ship.sections = SECTIONS.pop(-1) # first ship init
+
+#MAIN GAME
 
 while placement(): # place the ships state
     for event in pygame.event.get():
